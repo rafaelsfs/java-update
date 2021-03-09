8e41a56677d548e3a6ca926d4f4a887b7f6afd40
Rafael Garcia <rafaelsfs@gmail.com>
2021-03-09 19:40:39 -0300

    fist version

--- java-update.sh
+++ java-update.sh
@@ -91,19 +91,17 @@ if [ "$AVAILABLE" != "$INSTALLED" ]; then
             java -version
 
             # java environment variables
-            if [ -e "/etc/bash.bashrc" ] then
-                sudo echo '#Java Eviroment' >> "/etc/bash.bashrc"
-                sudo echo 'JAVA_HOME=/opt/java' >> "/etc/bash.bashrc"
-                sudo echo 'JDK_HOME=$JAVA_HOME' >> "/etc/bash.bashrc"
-                sudo echo 'PATH=$PATH:$JAVA_HOME/bin' >> "/etc/bash.bashrc"
-                sudo echo 'export JAVA_HOME' >> "/etc/bash.bashrc"
-                sudo echo 'export PATH' >> "/etc/bash.bashrc"
-                source /etc/bash.bash
+            if [ -e "~/bashrc" ] then
+                echo '#Java Eviroment' >> "~/bashrc"
+                echo 'JAVA_HOME=/opt/java' >> "~/bashrc"
+                echo 'JDK_HOME=$JAVA_HOME' >> "~/bashrc"
+                echo 'PATH=$PATH:$JAVA_HOME/bin' >> "~/bashrc"
+                echo 'export JAVA_HOME' >> "~/bashrc"
+                echo 'export PATH' >> "~/bashrc"
+                source ~/bashrc
                 echo $JAVA_HOME
                 echo $JDK_HOME
-                echo $PATH
-            else
-                echo 'teste'    
+                echo $PATH  
             fi    
         fi
 else
