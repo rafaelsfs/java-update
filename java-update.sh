a6b5dbd3ba256cae6bb93bb9147249ec4f16e2ff
Rafael Garcia <rafaelsfs@gmail.com>
2021-03-09 19:12:02 -0300

    fist version

--- java-update.sh
+++ java-update.sh
@@ -14,7 +14,7 @@ INSTALLED=`cat /opt/java/release |grep -E 'JAVA_VERSION=' |awk -F"\"" '{print $2
 
 #Mostra as versoes instalada e disponivel no site
 echo -e '\033[1;34m Installed version: '$INSTALLED
-echo -e '\033[1;36m Latest version...: '$AVAILABLE '/n'
+echo -e '\033[1;36m Latest version...: '$AVAILABLE '\n'
 
 #Save download link
 LINK=`curl -s https://jdk.java.net/$MAJOR/ |grep -E '*_linux-x64_bin.tar.gz">' |awk -F"\"" '{print $2}'`
@@ -26,7 +26,7 @@ ARQ=`curl -s https://jdk.java.net/$MAJOR/ |grep -E '*_linux-x64_bin.tar.gz">' |a
 if [ "$AVAILABLE" != "$INSTALLED" ]; then
 
     #shows message that they are different and that will update
-    echo -e '\033[1;31m The installed version is out of date downloading the new version \033[0m /n'
+    echo -e '\033[1;31m The installed version is out of date downloading the new version \033[0m \n'
 
         #Checks download file already exists
         if [ -e "$ARQ" ]; then        
@@ -77,7 +77,7 @@ if [ "$AVAILABLE" != "$INSTALLED" ]; then
             #Configures Ubuntu update-alteranatives to use the new version
             sudo update-alternatives --install /usr/bin/java java /opt/java/bin/java 1
 
-            #Checks whether update-alternatives is configured
+            #Checks whether update-alternatives is configu
             sudo update-alternatives --display java
 
             #Configures Ubuntu update-alteranatives to use the new version
