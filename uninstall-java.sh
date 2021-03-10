e32ca72f55e044805a36570eca5c95d243fd2bf0
Rafael Garcia <rafaelsfs@gmail.com>
2021-03-09 21:24:08 -0300

    fist version

--- uninstall-java.sh
+++ uninstall-java.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 echo -e '\033[1;31m Unstaling...'
+
+#Remove Ubuntu update-alteranatives
 sudo update-alternatives --remove java /opt/java/bin/java
 sudo update-alternatives --remove javac /opt/java/bin/javac
 cd /opt
+
+#remove symbolic link
 sudo rm java
+
+#remove java DIR
 sudo rm -Rf jdk*
+
 cd $HOME
+
+#delete java tar.gz file
 rm openjdk*
+
+#remove java-update directory
 rm -Rf java-update
+
+#restore bashrc file
 mv .bashrc.bak .bashrc
+
+#load bashrc original config
 source .bashrc
 echo -e '\033[1;32m Uninstall complete'
